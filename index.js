616f6a611b0b4e3c0cfd95213fc8f4b3bea69024
Jana Rangasamy <janagr7@gmail.com>
2022-10-12 16:22:58 +0530

    updated index.js

--- index.js
+++ index.js
@@ -2,4 +2,26 @@
 
 module.exports = {
 
+  average: require('./lib/average'),
+  correlationCoefficient: require('./lib/correlationCoefficient'),
+  count: require('./lib/count'),
+  covariance: require('./lib/covariance'),
+  devSq: require('./lib/devSq'),
+  forecast: require('./lib/forecast'),
+  frequencyN: require('./lib/frequencyN'),
+  frequencyS: require('./lib/frequencyS'),
+  intercept: require('./lib/intercept'),
+  large: require('./lib/large'),
+  max: require('./lib/max'),
+  median: require('./lib/median'),
+  min: require('./lib/min'),
+  mode: require('./lib/mode'),
+  rankAvg: require('./lib/rankAvg'),
+  rankEq: require('./lib/rankEq'),
+  slope: require('./lib/slope'),
+  small: require('./lib/small'),
+  stdDev: require('./lib/stdDev'),
+  sum: require('./lib/sum'),
+  varP: require('./lib/varP')
+
 }
